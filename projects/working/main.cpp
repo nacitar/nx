06cdf4b553b0452daad8429bfcc38689454576c7
Jacob McIntosh <nacitar@ubercpp.com>
2013-04-15 13:54:08 -0500

    Added application class.

--- projects/working/main.cpp
+++ projects/working/main.cpp
@@ -1,8 +1,15 @@
 #include <nx/core.hpp>
+#include <nx/application.hpp>
 
-int main(int argc,char*argv[])
-{
-	nx::int_t<32> x;
-  x=0;
-	return x;
-}
+#include <iostream>
+// Create my application
+struct MyApplication : nx::Application {
+  int main() {
+    nx::uint_least_t<32> x;
+    std::cout << sizeof(x) << std::endl;
+    return 0;
+  }
+};
+
+// Set this as the default application
+nx::Application& nx::application = *new MyApplication();

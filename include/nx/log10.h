e1ebd2db84097b882430e126c3e19a2b4d323263
Jacob McIntosh <nacitar@ubercpp.com>
2013-07-30 10:37:46 -0500

    Doxygen updates for constant.h and log10.h

--- include/nx/log10.h
+++ include/nx/log10.h
@@ -14,8 +14,8 @@
 // limitations under the License.
 //
 
-/// @file
-/// Provides a function to calculate the log10 of an unsigned integer.
+/// @file log10.h
+/// @brief Provides a function to calculate the log10 of an unsigned integer.
 
 #ifndef INCLUDE_NX_LOG10_H_
 #define INCLUDE_NX_LOG10_H_
@@ -23,77 +23,86 @@
 #include "nx/core.h"
 #include "nx/constant.h"
 
+/// @brief Library namespace.
 namespace nx {
+  /// @cond nx_detail
   namespace detail {
-    template <unsigned int uVersion, class T>
-    constexpr EnableIf<
-      All<std::is_unsigned<T>, Bool<uVersion == 64>>,
-    unsigned int> log10(T v) {
-      using constant::pow10_64;
-      return  (v < pow10_64[10]) ?
-          (v < pow10_64[5]) ?
-            (v < pow10_64[2]) ?
-              (v < pow10_64[1]) ? 0 : 1
+    /// @cond nx_detail_version
+    namespace version {
+      /// @brief 32-bit version
+      template <unsigned int uVersion, class T>
+      constexpr EnableIf<
+        All<std::is_unsigned<T>, Bool<uVersion == 32>>,
+      unsigned int> log10(T value) {
+        using constant::pow10_32;
+        return  (value < pow10_32[5]) ?
+            (value < pow10_32[2]) ?
+              (value < pow10_32[1]) ? 0 : 1
             :
-            (v < pow10_64[4]) ?
-              (v < pow10_64[3]) ? 2 : 3
+            (value < pow10_32[4]) ?
+              (value < pow10_32[3]) ? 2 : 3
             : 4
           :
-          (v < pow10_64[7]) ?
-            (v < pow10_64[6]) ? 5 : 6
+          (value < pow10_32[7]) ?
+            (value < pow10_32[6]) ? 5 : 6
           :
-          (v < pow10_64[9]) ?
-            (v < pow10_64[8]) ? 7 : 8
-          : 9
-        :
-        (v < pow10_64[15]) ?
-          (v < pow10_64[12]) ?
-            (v < pow10_64[11]) ? 10 : 11
+          (value < pow10_32[9]) ?
+            (value < pow10_32[8]) ? 7 : 8
+          : 9;
+      }
+      /// @brief 64-bit version
+      template <unsigned int uVersion, class T>
+      constexpr EnableIf<
+        All<std::is_unsigned<T>, Bool<uVersion == 64>>,
+      unsigned int> log10(T value) {
+        using constant::pow10_64;
+        return  (value < pow10_64[10]) ?
+            log10<32>(static_cast<uint_least32_t>(value))
           :
-          (v < pow10_64[14]) ?
-            (v < pow10_64[13]) ? 12 : 13
-          : 14
-        :
-        (v < pow10_64[17]) ?
-          (v < pow10_64[16]) ? 15 : 16
-        :
-        (v < pow10_64[19]) ?
-          (v < pow10_64[18]) ? 17 : 18
-        : 19;
+          (value < pow10_64[15]) ?
+            (value < pow10_64[12]) ?
+              (value < pow10_64[11]) ? 10 : 11
+            :
+            (value < pow10_64[14]) ?
+              (value < pow10_64[13]) ? 12 : 13
+            : 14
+          :
+          (value < pow10_64[17]) ?
+            (value < pow10_64[16]) ? 15 : 16
+          :
+          (value < pow10_64[19]) ?
+            (value < pow10_64[18]) ? 17 : 18
+          : 19;
+      }
+    }  // namespace version
+    /// @endcond
+    /// @brief [0,32]-bit selector
+    template <class T>
+    inline constexpr EnableIf<
+        All<std::is_unsigned<T>, BitRange<T, 0, 32>>,
+    unsigned int> log10(T value) {
+      return version::log10<32>(value);
     }
-    template <unsigned int uVersion, class T>
-    constexpr EnableIf<
-      All<std::is_unsigned<T>, Bool<uVersion == 32>>,
-    unsigned int> log10(T v) {
-      using constant::pow10_32;
-      return  (v < pow10_32[5]) ?
-          (v < pow10_32[2]) ?
-            (v < pow10_32[1]) ? 0 : 1
-          :
-          (v < pow10_32[4]) ?
-            (v < pow10_32[3]) ? 2 : 3
-          : 4
-        :
-        (v < pow10_32[7]) ?
-          (v < pow10_32[6]) ? 5 : 6
-        :
-        (v < pow10_32[9]) ?
-          (v < pow10_32[8]) ? 7 : 8
-        : 9;
+    /// @brief [33,64]-bit selector
+    template <class T>
+    inline constexpr EnableIf<
+        All<std::is_unsigned<T>, BitRange<T, 33, 64>>,
+    unsigned int> log10(T value) {
+      return version::log10<64>(value);
     }
   }  // namespace detail
+  /// @endcond
 
+  /// @brief Determines the log10 of the value.
+  ///
+  /// @tparam T The type of the passed value.
+  /// @param value The value to examine.
+  ///
+  /// @return The log10 of the value, useful for determining the number of
+  /// digits in a decimal number.
   template <class T>
-  inline constexpr EnableIf<
-      All<std::is_unsigned<T>, BitRange<T, 0, 32>>,
-  unsigned int> log10(T v) {
-    return detail::log10<32>(v);
-  }
-  template <class T>
-  inline constexpr EnableIf<
-      All<std::is_unsigned<T>, BitRange<T, 33, 64>>,
-  unsigned int> log10(T v) {
-    return detail::log10<64>(v);
+  inline constexpr unsigned int log10(T value) {
+    return detail::log10(value);
   }
 
 }  // namespace nx

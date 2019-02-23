d222a50de6e2056678ac7535d42589605b67d676
Jacob McIntosh <nacitar@ubercpp.com>
2019-02-23 01:14:45 -0600

    Better check_style.py

--- check_style.py
+++ check_style.py
@@ -22,52 +22,72 @@ import sys
 if sys.version_info < (3, 6):
     raise RuntimeError('Python 3.6+ is required.')
 
-import pathlib, os
+import pathlib
+import os
 this_file = pathlib.Path(__file__).resolve()
 
 import importlib.util
 cpplint_path = pathlib.Path(
         this_file.parent, 'external/3rdparty/cpplint/cpplint.py')
-cpplint_spec=importlib.util.spec_from_file_location('cpplint', cpplint_path)
-cpplint=importlib.util.module_from_spec(cpplint_spec)
+cpplint_spec = importlib.util.spec_from_file_location('cpplint', cpplint_path)
+cpplint = importlib.util.module_from_spec(cpplint_spec)
 cpplint_spec.loader.exec_module(cpplint)
 
+def listize(obj):
+    """ If obj is iterable and not a string, returns a new list with the same
+    contents.  Otherwise, returns a new list with obj as its only element.
+    """
+    if not isinstance(obj, str):
+        try:
+            return list(obj)
+        except:
+            pass
+    return [obj]
 
-def main():
-    code_dirs = [
-        'src/',
-        'test/',
-        'include/nx/',
-        'external/nx-core/include/nx'
-    ]
-
-    retval = 0
-    for path in [os.path.join(this_file.parent, code_dir) for code_dir in code_dirs]:
+def iter_children_with_extension(root_dirs, extension_list):
+    root_dirs = listize(root_dirs)
+    print(repr(root_dirs))
+    extension_list = [x.lower() for x in listize(extension_list)]
+    for path in root_dirs:
         for root, dirs, files in os.walk(path):
             for filename in files:
-                if pathlib.Path(filename).suffix.lower() in [ '.cc', '.h' ]:
-                    full_path = pathlib.Path(root, filename)
-                    if full_path.parts[len(this_file.parts)-1] == 'external':
-                        cpplint_repository = pathlib.Path(
-                                *full_path.parts[:len(this_file.parts)+1])
-                    else:
-                        cpplint_repository = this_file.parent
-                    try:
-                        original_argv = sys.argv
-                        sys.argv = [
-                                str(cpplint_path),
-                                '--quiet',
-                                '--filter=-build/c++11',
-                                f'--repository={cpplint_repository}',
-                                str(full_path)
-                        ]
-                        cpplint.main()
-                    except SystemExit as exit:
-                        if exit.code != 0:
-                            retval += 1
-                    finally:
-                        sys.argv = original_argv
+                if pathlib.Path(filename).suffix.lower() in extension_list:
+                    yield pathlib.Path(root, filename)
+code_dirs = [
+    'src/',
+    'test/',
+    'include/nx/',
+    'external/nx-core/include/nx'
+]
+
+def main():
+    retval = 0
+    for full_path in iter_children_with_extension(
+            [pathlib.Path(this_file.parent, code_dir)
+                for code_dir in code_dirs],
+            ['.cc', '.h']):
+        if full_path.parts[len(this_file.parts)-1] == 'external':
+            cpplint_repository = pathlib.Path(
+                    *full_path.parts[:len(this_file.parts)+1])
+        else:
+            cpplint_repository = this_file.parent
+        try:
+            original_argv = sys.argv
+            sys.argv = [
+                    str(cpplint_path),
+                    '--quiet',
+                    '--filter=-build/c++11',
+                    f'--repository={cpplint_repository}',
+                    str(full_path)
+            ]
+            print(repr(sys.argv))
+            cpplint.main()
+        except SystemExit as exit:
+            if exit.code != 0:
+                retval += 1
+        finally:
+            sys.argv = original_argv
     return retval
 
-if __name__== '__main__':
+if __name__ == '__main__':
     sys.exit(main())
